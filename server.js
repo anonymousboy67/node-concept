fb4f4f79670d14dbb35eb3b397e455400cc26db8
anonymousboy67 <aashishad67@gmail.com>
2025-02-03 10:21:16 +0545

    main

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express=require("express");
 const{configureCors}=require("./config/corsConfig");
+const { requestLogger, addTimeStamp } = require("./middleware/customMiddleware");
 
 const app=express();
 const PORT=process.env.PORT || 3000;
+
+app.use(requestLogger);
+app.use(addTimeStamp);
 
 
 //express json middleware
